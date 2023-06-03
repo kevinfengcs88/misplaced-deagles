86746e6c1b4828114f7c2ba9c2c1626a5b2645d4
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-06-03 14:02:45 -0400

    population.csv is now populated with newest data every time the program is run

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from soup import *
 import pandas as pd
+import os.path
+import csv
 
 val_count = scrape_player_counter_val()
 csgo_count = scrape_steamcharts_csgo()
 
-print(val_count)
-print(csgo_count)
+path = 'data/population.csv'
 
-f = open('data/population.csv', 'w')
+new_data = str(val_count) + ',' + str(csgo_count)
+
+with open(path, 'a') as f:
+    if os.path.getsize(path) == 0:
+        pass
+    else:
+        f.write('\n')
+    f.write(new_data)
+
+print('====================')
+print('After:')
+df = pd.read_csv(path, header=None)
+print(df)
