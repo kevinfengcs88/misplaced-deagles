f99045ac10784fb5d71e4cb9c1f316b569a84cb3
kevinfengcs88 <kevinfeng.cs88@gmail.com>
2023-06-03 19:55:02 -0400

    Program now writes the month, day, year, and hour to population.csv

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ from soup import *
 import pandas as pd
 import os.path
 import csv
+from datetime import datetime
 
 val_count = scrape_player_counter_val()
 csgo_count = scrape_steamcharts_csgo()
 
+now = datetime.now()
+current_month = now.month
+current_day = now.day
+current_year = now.year
+current_hour = now.hour
+current = str(current_month) + '/' + str(current_day) + '/' + str(current_year) + '/' + str(current_hour)
+
+new_data = current + ',' + str(val_count) + ',' + str(csgo_count)
+
 path = 'data/population.csv'
-
-new_data = str(val_count) + ',' + str(csgo_count)
-
 with open(path, 'a') as f:
     if os.path.getsize(path) == 0:
         pass
@@ -18,6 +25,6 @@ with open(path, 'a') as f:
     f.write(new_data)
 
 print('====================')
-print('After:')
+print('population.csv:')
 df = pd.read_csv(path, header=None)
 print(df)
